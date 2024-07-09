fddecd74ae5558650c243a8cc415ea395dd02eb5
VxPYr <vishnujaswal2004@gmail.com>
2024-07-09 11:58:23 +0530

    create likeController TODOs

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -13,14 +13,23 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
     
     const like = await Like.findOne({ video: videoId, likedBy: req.user._id })
 
-    if(like){
-        like.deleteOne();
-        return res.status(200).json(new ApiResponse(200, {}, "Like removed"))
+    if(!like){
+        const newLike = await Like.create({ video: videoId, likedBy: req.user._id});
+
+        return res.status(201).json(new ApiResponse(201, newLike, "Video liked"))
     }
 
-    const likedVideo = await Like.create({ video: videoId, likedBy: req.user._id });
+    const removeLike = await like.deleteOne();
+    return res.status(200).json(new ApiResponse(200, removeLike, "Video unliked");
 
-    return res.status(201).json(new ApiResponse(200, likedVideo, "Liked"))
+    // if(like){
+    //     like.deleteOne();
+    //     return res.status(200).json(new ApiResponse(200, {}, "Video like removed"))
+    // }
+
+    // const likedVideo = await Like.create({ video: videoId, likedBy: req.user._id });
+
+    // return res.status(201).json(new ApiResponse(201, likedVideo, "Video liked"))
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
@@ -30,17 +39,41 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Invalid comment id");
     }
 
-    
+    const like = Like.findOne({ comment: commentId, likedBy: req.user._id})
+    console.log(like);
+
+    if(like){
+        like.deleteOne();
+        return res.status(200).json(new ApiResponse(200, {}, "Comment like removed."))
+    }
+
+    const likedComment = Like.create({ comment: commentId, likedBy: req.user._id })
+    return res.status(201).json(new ApiResponse(200, likedComment, "Comment liked"))
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    if(!isValidObjectId){
+        throw new ApiError(400, "Invalid tweet id");
+    }
+    const like = await Like.findOne({ tweet: tweetId, likedBy: req.user._id })
+
+    if(like){
+        like.deleteOne();
+        return res.status(200).json(new ApiResponse(200, {}, "Tweet like removed."))
+    }
+
+    const likedTweet = await Like.create({ tweet: tweetId, likedBy: req.user._id });
+    return res.status(201).json(new ApiResponse(201, likedTweet, "Tweet liked"))
 }
 )
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    if(!isValidObjectId){
+        throw new ApiError(400, "Invalid video id")
+    }
 })
 
 export {
